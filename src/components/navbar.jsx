8f45c05a634c584c9f5c2e452b586f13bc7f65a8
KeScript <nnamanikester@gmail.com>
2020-03-15 00:25:05 +0100

    Destructured the props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounter}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = ({ totalCounter }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary">{totalCounter}</span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
